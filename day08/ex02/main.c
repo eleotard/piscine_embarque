35e52e0f6b866b5d771eb53a378f65931765f29a
Elsie LEOTARD <eleotard@made-f0Ar6s8.clusters.42paris.fr>
2024-04-25 17:27:33 +0200

    ex02

---
+++ day08/ex02/main.c
@@ -0,0 +1,110 @@
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+
+/*SPI == Serial Peripheral Interface,
+data transfer protocol for microcontrollers*/
+
+#define SPI_DDR DDRB
+#define SS      PINB2 //Slave Select Line
+#define MOSI    PINB3 //Master Out Slave In line
+#define MISO    PINB4 //Master In Slave Out line
+#define SCK     PINB5 //Shift Clock
+
+void SPI_init()
+{
+    // set CS, MOSI and SCK to output, miso is an input
+    SPI_DDR |= (1 << SS) | (1 << MOSI) | (1 << SCK);
+    // enable SPI, set as master, and clock to fosc/128
+    SPCR = (1 << SPE) | (1 << MSTR) | (1 << SPR1) | (1 << SPR0);
+}
+
+void SPI_masterTransmitByte(uint8_t data)
+{
+    // load data into data register
+    SPDR = data;
+    // Wait for transmission complete //polling
+	//termine quand SPIF passe a 1
+    while(!(SPSR & (1 << SPIF)));
+}
+
+void	send_colordata_to_D6(uint8_t r, uint8_t g, uint8_t b)
+{
+	for (uint8_t i = 0; i < 4; i++)
+		SPI_masterTransmitByte(0);
+	SPI_masterTransmitByte(255);
+	SPI_masterTransmitByte(b);
+	SPI_masterTransmitByte(g);
+	SPI_masterTransmitByte(r);
+	SPI_masterTransmitByte(255);
+	SPI_masterTransmitByte(0);
+	SPI_masterTransmitByte(0);
+	SPI_masterTransmitByte(0);
+	SPI_masterTransmitByte(255);
+	SPI_masterTransmitByte(0);
+	SPI_masterTransmitByte(0);
+	SPI_masterTransmitByte(0);
+	for (uint8_t j = 0; j < 4; j++)
+		SPI_masterTransmitByte(255);
+}
+
+void	send_colordata_to_D7(uint8_t r, uint8_t g, uint8_t b)
+{
+	for (uint8_t i = 0; i < 4; i++)
+		SPI_masterTransmitByte(0);
+	SPI_masterTransmitByte(255);
+	SPI_masterTransmitByte(0);
+	SPI_masterTransmitByte(0);
+	SPI_masterTransmitByte(0);
+	SPI_masterTransmitByte(255);
+	SPI_masterTransmitByte(b);
+	SPI_masterTransmitByte(g);
+	SPI_masterTransmitByte(r);
+	SPI_masterTransmitByte(255);
+	SPI_masterTransmitByte(0);
+	SPI_masterTransmitByte(0);
+	SPI_masterTransmitByte(0);
+	for (uint8_t j = 0; j < 4; j++)
+		SPI_masterTransmitByte(255);
+}
+
+void	send_colordata_to_D8(uint8_t r, uint8_t g, uint8_t b)
+{
+	for (uint8_t i = 0; i < 4; i++)
+		SPI_masterTransmitByte(0);
+	SPI_masterTransmitByte(255);
+	SPI_masterTransmitByte(0);
+	SPI_masterTransmitByte(0);
+	SPI_masterTransmitByte(0);
+	SPI_masterTransmitByte(255);
+	SPI_masterTransmitByte(0);
+	SPI_masterTransmitByte(0);
+	SPI_masterTransmitByte(0);
+	SPI_masterTransmitByte(255);
+	SPI_masterTransmitByte(b);
+	SPI_masterTransmitByte(g);
+	SPI_masterTransmitByte(r);
+	for (uint8_t j = 0; j < 4; j++)
+		SPI_masterTransmitByte(255);
+}
+
+int main()
+{
+	SPI_init();
+
+	while (1)
+	{
+		_delay_ms(83);
+		send_colordata_to_D6(255, 255, 255);
+		_delay_ms(250);
+		send_colordata_to_D6(0, 0, 0);
+		_delay_ms(83);
+		send_colordata_to_D7(255, 255, 255);
+		_delay_ms(250);
+		send_colordata_to_D7(0, 0, 0);
+		_delay_ms(84);
+		send_colordata_to_D8(255, 255, 255);
+		_delay_ms(250);
+		send_colordata_to_D8(0, 0, 0);
+	}
+}
